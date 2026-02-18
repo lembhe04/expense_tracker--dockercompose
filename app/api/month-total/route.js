8f01e42fc409e301fc3293b9c0c5c770b9fcf7d9
lembhe04 <prachi2003nov04@gmail.com>
2026-02-18 16:32:28 +0530

    total for expense

--- app/api/month-total/route.js
+++ app/api/month-total/route.js
@@ -1,15 +1,19 @@
 import { NextResponse } from "next/server";
 import pool from "@/lib/db";
 
-export async function GET() {
+export async function GET(req) {
   try {
+
+    const { searchParams } = new URL(req.url);
+    const user_id = searchParams.get("user_id");
 
     const result = await pool.query(`
       SELECT COALESCE(SUM(amount),0) AS total
       FROM expenses
-      WHERE DATE_TRUNC('month', created_at) =
-            DATE_TRUNC('month', CURRENT_DATE)
-    `);
+      WHERE user_id=$1
+      AND DATE_TRUNC('month', created_at) =
+          DATE_TRUNC('month', CURRENT_DATE)
+    `,[user_id]);
 
     return NextResponse.json({
       total: result.rows[0].total
