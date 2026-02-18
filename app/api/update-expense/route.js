7179982856a36dab74d901fe50d66ab7e8a8baeb
lembhe04 <prachi2003nov04@gmail.com>
2026-02-18 16:31:49 +0530

    update record logic and ui changed

--- app/api/update-expense/route.js
+++ app/api/update-expense/route.js
@@ -2,22 +2,34 @@ import { NextResponse } from "next/server";
 import pool from "@/lib/db";
 
 export async function PUT(req) {
+
   try {
 
-    const { id, amount, category, description } = await req.json();
+    const { id, amount, category, description, user_id } = await req.json();
+
+    if (!id || !user_id) {
+      return NextResponse.json(
+        { error: "Missing id or user" },
+        { status: 400 }
+      );
+    }
 
     await pool.query(
       `UPDATE expenses 
        SET amount=$1, category=$2, description=$3
-       WHERE id=$4`,
-      [amount, category, description, id]
+       WHERE id=$4 AND user_id=$5`,
+      [amount, category, description, id, user_id]
     );
 
     return NextResponse.json({ message: "Expense updated" });
 
   } catch (err) {
+
     console.error(err);
-    return NextResponse.json({ error: "Update failed" }, { status: 500 });
+
+    return NextResponse.json(
+      { error: "Update failed" },
+      { status: 500 }
+    );
   }
 }
-
